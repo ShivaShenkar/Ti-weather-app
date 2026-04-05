e63d29587191465642161e7fd4c6bc582d536bde
ShivaShenkar <109809637+ShivaShenkar@users.noreply.github.com>
2026-04-05 15:56:06 +0300

    coded build.py

--- server/server.py
+++ server/server.py
@@ -1,48 +1,39 @@
+import sys
 from flask import Flask, send_from_directory
 import webbrowser
 import threading
 import os
-
-SERVER_DIR = os.path.dirname(os.path.abspath(__file__))
-CLIENT_DIR = os.path.normpath(os.path.join(SERVER_DIR, "..", "client"))
-CLIENT_BUILD_PATH = os.path.join(CLIENT_DIR,"dist","weather-app","browser")
+from config import UI_PATH
 
 
-# Debug: Print the resolved path (you can remove this later)
-print(f"Server directory: {SERVER_DIR}\n")
-print(f"Angular path resolved to: {CLIENT_BUILD_PATH }\n")
-print(f"Path exists: {os.path.exists(CLIENT_BUILD_PATH )}\n")
+def get_ui_path():
+    if getattr(sys, 'frozen', False):
+        return os.path.join(sys._MEIPASS, 'ui')
+    return UI_PATH
 
 
-client_app = Flask(__name__, static_folder=CLIENT_BUILD_PATH )
+app = Flask(__name__)
 
-@client_app.route("/")
+
+@app.route("/")
 def home():
-    return send_from_directory(CLIENT_BUILD_PATH , "index.html")
+    ui_path = get_ui_path()
+    return send_from_directory(ui_path, "index.html")
 
 
-
-
-@client_app.route("/<path:path>")
+@app.route("/<path:path>")
 def static_files(path):
-   # Check if the file exists, otherwise serve index.html for Angular routes
-    file_path = os.path.join(CLIENT_BUILD_PATH , path)
+    ui_path = get_ui_path()
+    file_path = os.path.join(ui_path, path)
     if os.path.isfile(file_path):
-        return send_from_directory(CLIENT_BUILD_PATH , path)
-    else:
-        # For Angular routes (SPA), serve index.html
-        return send_from_directory(CLIENT_BUILD_PATH , "index.html")
-
-
-
+        return send_from_directory(ui_path, path)
+    return send_from_directory(ui_path, "index.html")
 
 
 def open_browser():
     webbrowser.open("http://localhost:8080")
 
 
-
 if __name__ == "__main__":
     threading.Timer(5, open_browser).start()
-    client_app.run(port=8080)
-
+    app.run(port=8080)
